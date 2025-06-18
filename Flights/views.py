1f2e0273ef4a42f0eef8f02e062ef7655f824c15
siphakwjhb024 <siphakwjhb024@student.wethinkcode.co.za>
2025-06-18 13:44:03 +0200

    Progress before repo creation

---
+++ Flights/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from . models import Flight
+# Create your views here.
+
+def index(request):
+    return render( request, "Flights/index.html", {
+        "flights": Flight.objects.all(),
+    } )
+
