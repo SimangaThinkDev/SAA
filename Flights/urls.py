c7eb566a0a961ecfa8f63a970d4392170c41936d
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-06-20 21:21:38 +0200

    Fixing paths for individual flight pages to be accessed with a link bak to the Home page in each

--- Flights/urls.py
+++ Flights/urls.py
@@ -1,10 +1,12 @@
 
-from django.urls import path
+from django.urls import path, reverse
+from django.http import HttpResponseRedirect
+# from django.
 from . import views
 
 app_name = 'flights'
 
 urlpatterns = [
     path( 'flights', views.index, name='index' ),
-    path( 'flights/<int:flight_id>', views.flight, name="flight" ),
+    path( 'flights/<int:flight_id>/', views.flight, name="flight" ),
 ]
