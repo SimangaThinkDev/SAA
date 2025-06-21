ae55f5c07f04b4fbd16f62589e3ad95e19581ea2
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-06-21 13:35:25 +0200

    Updating Admin interface for more changeability

--- Flights/admin.py
+++ Flights/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Airport, Flight, Passenger
 
+
+
 # Register your models here.
-admin.site.register(Flight)
+
+# Tell django of what fields should be displayed in the admin interface
+
+class FlightAdmin( admin.ModelAdmin ):
+    list_display = ( "id", "origin", "destination", "duration" )
+
+
+class PassengerAdmin( admin.ModelAdmin ):
+    filter_horizontal = ["flights"]
+
+
+admin.site.register(Flight, FlightAdmin)
 admin.site.register(Airport)
-admin.site.register(Passenger)
+admin.site.register(Passenger, PassengerAdmin)
+
